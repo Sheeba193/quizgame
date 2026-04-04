dd89816431385b7de9f7e0ef672c59121cd054d6
Sheeba193 <bethkerubo193@gmail.com>
2026-04-04 23:43:56 +0300

    data elements

---
+++ script.js
@@ -0,0 +1,16 @@
+//dom elements
+const startScreen = document.getElementById('start-screen');
+const startBtn = document.getElementById('start-btn');
+const quizScreen = document.getElementById('quiz-screen');
+const resultScreen = document.getElementById('result-screen');
+const submitBtn = document.getElementById('submit-btn');
+const restartButton = document.getElementById('restart-btn');
+const questionText = document.getElementById('question-text');
+const answersContainer = document.getElementById('answers-container');
+const currentQuestionSpan = document.getElementById('current-question');
+const totalQuestionsSpan = document.getElementById('total-questions');
+const scoreSpan = document.getElementById('score');
+const finalScoreSpan = document.getElementById('final-score');
+const maxScoreSpan = document.getElementById('max-score');
+const resultMessage = document.getElementById('result-message');
+const progressBar = document.getElementById('progress-bar');
